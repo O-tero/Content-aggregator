420ab72ef58984ef4452c74b2b957e68e46441f9
Otero <oteronickbrian@gmail.com>
2024-06-24 15:25:48 +0300

    running custom comands

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -1,15 +1,28 @@
-from django.core.management.base import BaseCommand
+# Standard Library
+import logging
 
+# Third Party
 import feedparser
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 from dateutil import parser
 
+# Django
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+# Models
 from podcasts.models import Episode
+
+logger = logging.getLogger(__name__)
 
 
 def save_new_episodes(feed):
     """Saves new episodes to the database.
 
-    Checks the episode GUID against the episodes currently stored in the
+    Checks the episode GUID agaist the episodes currently stored in the
     database. If not found, then a new `Episode` is added to the database.
 
     Args:
@@ -33,7 +46,7 @@ def save_new_episodes(feed):
 
 
 def fetch_realpython_episodes():
-    """Fetches new episodes from RSS for The Real Python Podcast."""
+    """Fetches new episodes from RSS for the Real Python Podcast."""
     _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
     save_new_episodes(_feed)
 
@@ -44,7 +57,54 @@ def fetch_talkpython_episodes():
     save_new_episodes(_feed)
 
 
+def delete_old_job_executions(max_age=604_800):
+    """Deletes all apscheduler job execution logs older than `max_age`."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
 class Command(BaseCommand):
+    help = "Runs apscheduler."
+
     def handle(self, *args, **options):
-        fetch_realpython_episodes()
-        fetch_talkpython_episodes()
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            fetch_realpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="The Real Python Podcast",  # Each job MUST have a unique ID
+            max_instances=1,
+            # Replaces existing and stops duplicates on restart of the app.
+            replace_existing=True,
+        )
+        logger.info("Added job: The Real Python Podcast.")
+
+        scheduler.add_job(
+            fetch_talkpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="Talk Python Feed",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: Talk Python Feed.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: Delete Old Job Executions.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
